9ea48712d036577cdfc88fc277c74c18e1194b93
Djouahra Yanis <yanis.djouahra@epitech.eu>
2026-02-21 16:46:10 +0100

    création des routes CRUD pour les livres modification de prettier et ajout de settings pour vscode

--- backend/app.js
+++ backend/app.js
@@ -1,54 +1,66 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 app.use(express.json());
 
-const Book = require("./models/Book");
-const User = require("./models/User");
+const Book = require('./models/Book');
+const User = require('./models/User');
 
 mongoose
   .connect(
-    "mongodb+srv://datauser1:user1234@cluster0.fmuonpl.mongodb.net/?appName=Cluster0",
+    'mongodb+srv://datauser1:user1234@cluster0.fmuonpl.mongodb.net/?appName=Cluster0',
   )
-  .then(() => console.log("Connexion à MongoDB réussie !"))
-  .catch((error) => console.log("Connexion à MongoDB échouée !", error));
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch((error) => console.log('Connexion à MongoDB échouée !', error));
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization",
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization',
   );
   res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS',
   );
   next();
 });
 
-app.post("/api/auth/signup", (req, res) => {
-  const user = new User({
-    email: req.body.email,
-    password: req.body.password,
+// Routes pour les livres
+
+app.post('/api/books', (req, res) => {
+  const book = new Book({
+    ...req.body,
   });
-  user
+  book
     .save()
-    .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
+    .then(() => res.status(201).json({ message: 'Livre enregistré !' }))
     .catch((error) => res.status(400).json({ error }));
 });
 
-app.post("/api/auth/login", (req, res) => {
-  User.findOne({ email: req.body.email })
-    .then((user) => {
-      if (!user) {
-        return res.status(401).json({ error: "Utilisateur non trouvé !" });
-      }
-      if (user.password !== req.body.password) {
-        return res.status(401).json({ error: "Mot de passe incorrect !" });
-      }
-      res.status(200).json({ userId: user._id });
-    })
-    .catch((error) => res.status(500).json({ error }));
+app.get('/api/books', (req, res) => {
+  Book.find()
+    .then((books) => res.status(200).json(books))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+app.get('/api/books/:id', (req, res) => {
+  Book.findOne({ _id: req.params.id })
+    .then((book) => res.status(200).json(book))
+    .catch((error) => res.status(404).json({ error }));
+});
+
+app.put('/api/books/:id', (req, res) => {
+  Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Livre modifié !' }))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+app.delete('/api/books/:id', (req, res) => {
+  Book.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Livre supprimé !' }))
+    .catch((error) => res.status(400).json({ error }));
 });
 
 module.exports = app;
