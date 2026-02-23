32dd9ab8970fd3d154698e8ee612ff09efec8e65
Djouahra Yanis <yanis.djouahra@epitech.eu>
2026-02-23 15:30:23 +0100

    création de la logique de connexion des utilisateurs

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcrypt');
 const User = require('../models/User');
 
-exports.signup = (req, res, next) => {
+exports.signup = (req, res) => {
   bcrypt
     .hash(req.body.password, 10)
     .then((hash) => {
@@ -17,4 +17,29 @@ exports.signup = (req, res, next) => {
     .catch((error) => res.status(500).json({ error }));
 };
 
-exports.login = () => {};
+exports.login = (req, res) => {
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        return res
+          .status(401)
+          .json({ message: 'Paire login/mot de passe incorrecte' });
+      }
+      return bcrypt
+        .compare(req.body.password, user.password)
+        .then((valid) => {
+          if (!valid) {
+            return res
+              .status(401)
+              .json({ message: 'Paire login/mot de passe incorrecte' });
+          }
+          return res.status(200).json({
+            // eslint-disable-next-line no-underscore-dangle
+            userId: user._id,
+            token: 'TOKEN',
+          });
+        })
+        .catch((error) => res.status(500).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
+};
