814388f62e6c5421281454f9e9d87dd2da5cbaba
Djouahra Yanis <yanis.djouahra@epitech.eu>
2026-04-19 17:22:05 +0200

    ajout d'icon personnalisé et changement du title de la page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
 
 import * as PropTypes from 'prop-types';
+import Logo from '../../images/logo.svg';
 import styles from './Header.module.css';
-import Logo from '../../images/Logo.png';
 
 function Header({ user, setUser }) {
   const navigate = useNavigate();
@@ -18,9 +17,48 @@ function Header({ user, setUser }) {
       <div className="container">
         <img src={Logo} alt="logo mpm vieu grimoire" />
         <ul>
-          <li><NavLink to="/" end className={({ isActive }) => (isActive ? styles.activeLink : undefined)}>Accueil</NavLink></li>
-          <li><NavLink to="/Ajouter" className={({ isActive }) => (isActive ? styles.activeLink : undefined)}>Ajouter un livre</NavLink></li>
-          <li>{!user ? <NavLink to="/Connexion" className={({ isActive }) => (isActive ? styles.activeLink : undefined)}>Se connecter</NavLink> : <span tabIndex={0} role="button" onKeyUp={disconnect} onClick={disconnect}>Se déconnecter</span> }</li>
+          <li>
+            <NavLink
+              to="/"
+              end
+              className={({ isActive }) =>
+                isActive ? styles.activeLink : undefined
+              }
+            >
+              Accueil
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/Ajouter"
+              className={({ isActive }) =>
+                isActive ? styles.activeLink : undefined
+              }
+            >
+              Ajouter un livre
+            </NavLink>
+          </li>
+          <li>
+            {!user ? (
+              <NavLink
+                to="/Connexion"
+                className={({ isActive }) =>
+                  isActive ? styles.activeLink : undefined
+                }
+              >
+                Se connecter
+              </NavLink>
+            ) : (
+              <span
+                tabIndex={0}
+                role="button"
+                onKeyUp={disconnect}
+                onClick={disconnect}
+              >
+                Se déconnecter
+              </span>
+            )}
+          </li>
         </ul>
       </div>
     </header>
